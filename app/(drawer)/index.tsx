4ed22ca8ff6978b1d0045615a49a0ba9264249dd
Angela <angelairuzubi97@gmail.com>
2026-03-01 13:29:29 +0100

    sabado_28/02

--- app/(drawer)/index.tsx
+++ app/(drawer)/index.tsx
@@ -1,32 +1,73 @@
-import { View, Text, Image, TextInput, Pressable, FlatList, Modal, ScrollView } from 'react-native';
-import { useState } from 'react';
 import { Ionicons } from '@expo/vector-icons';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import React, { useState } from 'react';
+import {
+  FlatList,
+  Image,
+  Modal,
+  Platform,
+  Pressable,
+  ScrollView,
+  Text,
+  TextInput,
+  View
+} from 'react-native';
 
 export default function Home() {
 
   const [modalVisible, setModalVisible] = useState(false);
+  const [taskModalVisible, setTaskModalVisible] = useState(false);
+
   const [newTask, setNewTask] = useState('');
   const [search, setSearch] = useState('');
 
+  const [selectedTask, setSelectedTask] = useState<any>(null);
+
   const [tasks, setTasks] = useState([
-    { id: '1', title: 'Hacer la cama', hora: '08:00' },
-    { id: '2', title: 'Lavar los platos', hora: '12:30' },
-    { id: '3', title: 'Sacar la basura', hora: '18:00' },
+    { id: '1', title: 'Hacer la cama', hora: '08:00', completed: false },
+    { id: '2', title: 'Lavar los platos', hora: '12:30', completed: false },
+    { id: '3', title: 'Sacar la basura', hora: '18:00', completed: false },
   ]);
+
+  const [showPicker, setShowPicker] = useState(false);
+  const [selectedTime, setSelectedTime] = useState<string | null>(null);
+  const [tempTime, setTempTime] = useState(new Date());
 
   const filteredTasks = tasks.filter(task =>
     task.title.toLowerCase().includes(search.toLowerCase())
   );
 
-  return (
-    <View style={{
-      flex: 1,
-      backgroundColor: '#ffffff',
-      paddingTop: 60,
-      paddingHorizontal: 20,
-    }}>
+  const handleTimeChange = (event: any, date?: Date) => {
+    if (date) {
+      const formatted = date.toLocaleTimeString([], {
+        hour: '2-digit',
+        minute: '2-digit',
+      });
+      setSelectedTime(formatted);
+    }
+    setShowPicker(false);
+  };
+  const today = new Date();
 
-      {/* HEADER */}
+const weekday = today.toLocaleDateString('es-ES', { weekday: 'long' });
+const day = today.getDate();
+const month = today.toLocaleDateString('es-ES', { month: 'long' });
+const year = today.getFullYear();
+
+const capitalize = (text: string) =>
+  text.charAt(0).toUpperCase() + text.slice(1);
+
+const formattedToday = `${capitalize(weekday)}, ${day} de ${capitalize(month)} de ${year}`;
+  
+    return (
+      <View style={{
+        flex: 1,
+        backgroundColor: '#ffffff',
+        paddingTop: 60,
+        paddingHorizontal: 20,
+      }}>
+
+
       <View style={{
         flexDirection: 'row',
         justifyContent: 'center',
@@ -42,10 +83,31 @@ export default function Home() {
           Mis Tareas
         </Text>
 
-        <Ionicons name="list" size={28} color="#A77BBE" />
+        <Ionicons style={{
+
+          marginTop: 10,
+        }} name="list" size={28} color="#A77BBE" />
       </View>
 
-      {/* SEARCH */}
+      <View style={{
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        marginBottom: 40,
+      }}>
+           <Text style={{
+              textAlign: 'left',
+              color: '#888',
+              marginTop: -25,
+              fontSize: 20
+            }}>
+              {formattedToday}
+            </Text>
+
+      </View>
+
+
+  
       <View style={{
         flexDirection: 'row',
         alignItems: 'center',
@@ -62,48 +124,48 @@ export default function Home() {
           onChangeText={setSearch}
           style={{ flex: 1, fontSize: 16 }}
         />
-
         <Ionicons name="search" size={20} color="#999" />
       </View>
 
-      {/* LIST */}
+      
       <FlatList
         data={filteredTasks}
         keyExtractor={(item) => item.id}
         contentContainerStyle={{ paddingBottom: 120 }}
         renderItem={({ item }) => (
-          <View style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            backgroundColor: '#f3f2f2',
-            padding: 18,
-            borderRadius: 15,
-            borderColor: '#eee',
-            borderWidth: 1,
-            marginBottom: 15,
-            elevation: 2,
-          }}>
-            <Text style={{ fontSize: 17, color: '#333' }}>
-              {item.title}
-            </Text>
-
+          <Pressable
+            onPress={() => {
+              setSelectedTask(item);
+              setTaskModalVisible(true);
+            }}
+          >
             <View style={{
               flexDirection: 'row',
+              justifyContent: 'space-between',
               alignItems: 'center',
-              gap: 8,
+              backgroundColor: item.completed ? '#DFF5E1' : '#f3f2f2',
+              padding: 18,
+              borderRadius: 15,
+              marginBottom: 15,
+              elevation: 2,
             }}>
-              <Text style={{ fontSize: 16, color: '#555' }}>
-                {item.hora}
+              <Text style={{
+                fontSize: 17,
+                color: '#333',
+                textDecorationLine: item.completed ? 'line-through' : 'none'
+              }}>
+                {item.title}
               </Text>
 
-              <Ionicons name="calendar-outline" size={20} color="#A77BBE" />
+              <Text style={{ color: '#555' }}>
+                {item.hora}
+              </Text>
             </View>
-          </View>
+          </Pressable>
         )}
       />
 
-      {/* BOTÓN AGREGAR */}
+
       <Pressable
         onPress={() => setModalVisible(true)}
         style={{
@@ -122,7 +184,7 @@ export default function Home() {
         <Ionicons name="add" size={36} color="#FFF" />
       </Pressable>
 
-      {/* MODAL */}
+
       <Modal visible={modalVisible} transparent animationType="slide">
         <View style={{
           flex: 1,
@@ -130,17 +192,14 @@ export default function Home() {
           justifyContent: 'center',
           alignItems: 'center',
         }}>
-
           <View style={{
             backgroundColor: '#F4F0F6',
             borderRadius: 20,
             padding: 20,
-            width: '50%',
+            width: '85%',
           }}>
-
             <ScrollView>
 
-              {/* CLOSE */}
               <Pressable
                 onPress={() => setModalVisible(false)}
                 style={{ alignSelf: 'flex-start' }}
@@ -148,7 +207,6 @@ export default function Home() {
                 <Ionicons name="close" size={28} color="#A77BBE" />
               </Pressable>
 
-              {/* INPUT */}
               <View style={{
                 flexDirection: 'row',
                 alignItems: 'center',
@@ -165,10 +223,31 @@ export default function Home() {
                   style={{ flex: 1, paddingVertical: 10 }}
                 />
 
-                <Ionicons name="calendar-outline" size={22} color="#A77BBE" />
+                <Pressable onPress={() => setShowPicker(true)}>
+                  <Ionicons name="calendar-outline" size={22} color="#A77BBE" />
+                </Pressable>
               </View>
 
-              {/* IMAGE */}
+              <Text style={{
+                marginTop: 10,
+                textAlign: 'center',
+                color: '#555'
+              }}>
+                {selectedTime
+                  ? `Hora seleccionada: ${selectedTime}`
+                  : "No has elegido hora"}
+              </Text>
+
+              {showPicker && (
+                <DateTimePicker
+                  value={tempTime}
+                  mode="time"
+                  is24Hour={true}
+                  display={Platform.OS === "ios" ? "spinner" : "default"}
+                  onChange={handleTimeChange}
+                />
+              )}
+
               <View style={{
                 justifyContent: 'center',
                 alignItems: 'center',
@@ -176,12 +255,11 @@ export default function Home() {
               }}>
                 <Image
                   source={require("../../assets/images/cine.png")}
-                  style={{ width: 220, height: 220 }}
+                  style={{ width: 180, height: 180 }}
                   resizeMode="contain"
                 />
               </View>
 
-              {/* ADD BUTTON */}
               <Pressable
                 onPress={() => {
                   if (newTask.trim() !== '') {
@@ -190,10 +268,13 @@ export default function Home() {
                       {
                         id: Date.now().toString(),
                         title: newTask,
-                        hora: '08:00',
+                        hora: selectedTime ?? "Sin hora",
+                        completed: false
                       }
                     ]);
+
                     setNewTask('');
+                    setSelectedTime(null);
                     setModalVisible(false);
                   }
                 }}
@@ -219,9 +300,77 @@ export default function Home() {
         </View>
       </Modal>
 
+   
+      <Modal visible={taskModalVisible} transparent animationType="slide">
+        <View style={{
+          flex: 1,
+          backgroundColor: 'rgba(0,0,0,0.3)',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+          <View style={{
+            backgroundColor: '#F4F0F6',
+            borderRadius: 20,
+            padding: 25,
+            width: '85%',
+            alignItems: 'center'
+          }}>
+
+            <Pressable
+              onPress={() => setTaskModalVisible(false)}
+              style={{ position: 'absolute', left: 15, top: 15 }}
+            >
+              <Ionicons name="close" size={26} color="#A77BBE" />
+            </Pressable>
+
+            <Text style={{
+              fontSize: 20,
+              marginBottom: 20,
+              marginTop: 20
+            }}>
+              {selectedTask?.title}
+            </Text>
+
+            <Image
+              source={require("../../assets/images/cine.png")}
+              style={{ width: 150, height: 150 }}
+              resizeMode="contain"
+            />
+
+            <Pressable
+              onPress={() => {
+                  setTasks(prev =>
+                    prev.filter(task => task.id !== selectedTask.id)
+                  );
+
+                  setTaskModalVisible(false);
+                }}
+                style={{
+                marginTop: 30,
+                backgroundColor: '#E5D9EE',
+                padding: 15,
+                borderRadius: 15,
+                width: '100%',
+                alignItems: 'center'
+              }}
+            >
+              <Text style={{
+                fontSize: 20,
+                color: '#A77BBE',
+                fontWeight: '600'
+              }}>
+                Realizada ✓
+              </Text>
+            </Pressable>
+
+          </View>
+        </View>
+      </Modal>
+
     </View>
   );
 }
+
 
 
 
